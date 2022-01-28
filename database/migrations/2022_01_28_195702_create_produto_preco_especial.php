4d8cc2149af2199f2297ff7ed74557af60a027b0
edusatelis <edusatelis@gmail.com>
2022-01-28 17:00:43 -0300

    Adding model and migrations from produto_preco_especial

---
+++ database/migrations/2022_01_28_195702_create_produto_preco_especial.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutoPrecoEspecial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_preco_especial', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('produto_id');
+            $table->string('preco');
+            $table->date('data_inicio');
+            $table->date('data_fim');
+
+            $table->timestamps();
+        });
+
+        Schema::table('produto_preco_especial', function (Blueprint $table) {
+            $table->foreign('produto_id')->references('id')->on('produto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_preco_especial');
+    }
+}
